5694b074ed320b930290c1f0b444e180684f76e4
tromoser <tromoser@gmail.com>
2022-11-19 15:13:15 -0800

    crud complete

--- gettogether/main_app/views.py
+++ gettogether/main_app/views.py
@@ -1,5 +1,7 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Event
 
 # Define the home view
@@ -15,3 +17,24 @@ def events_index(request):
   return render(request, 'events/index.html', {
     'events': events
   })
+
+def events_detail(request, event_id):
+  event = Event.objects.get(id=event_id)
+  return render(request, 'events/detail.html', {
+    'event': event
+  })
+
+# class EventDetail(DetailView):
+#   model = Event
+
+class EventCreate(CreateView):
+  model = Event
+  fields = '__all__'
+
+class EventUpdate(UpdateView):
+  model = Event
+  fields = '__all__'
+
+class EventDelete(DeleteView):
+  model = Event
+  success_url = '/events'
