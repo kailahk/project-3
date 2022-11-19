08f79d9076e3d77d5a7484a94b37cc440f8e01fb
Cormac Pujals <cmpujals@gmail.com>
2022-11-19 13:48:45 -0800

    adds about and index

--- gettogether/main_app/models.py
+++ gettogether/main_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
 
-# Create your models here.
+# class Comment(models.Model):
+#   content = models.CharField(max_length=150)
+#   created_at = models.DateTimeField(auto_now_add=True)
+#   updated_at = models.DateTimeField(auto_now=True)
+
+#   def __str__(self):
+#     return 
+
+
+class Event(models.Model):
+  title = models.CharField(max_length=40)
+  datetime = models.DateField()
+  description = models.TextField(max_length=1500)
+  address = models.CharField(max_length=100)
+  neighborhood = models.CharField(max_length=50)
+  city = models.CharField(max_length= 30)
+  # comments = models.ManyToManyField(Comment)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return f'{self.title} ({self.id})'
+
+  def get_absolulte_url(self):
+    return reverse('detail', kwargs={'event_id': self.id})
--- gettogether/main_app/urls.py
+++ gettogether/main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('events/', views.events_index, name='index'),
 ]
